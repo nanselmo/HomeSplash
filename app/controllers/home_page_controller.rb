166f6f2be68f81cd64b8a6fec166500893f58b5f
Nicki Anselmo <nickianselmo@gmail.com>
2015-03-11 21:03:42 -0500

    initial commit

---
+++ app/controllers/home_page_controller.rb
@@ -0,0 +1,20 @@
+class HomePageController < ApplicationController
+  def index
+    time = Time.now
+    #uses the stamp method to help format it to match the string "3:00AM"
+    @current_time = time.stamp("3:00 AM")
+    @greet_message = case Time.now.hour
+                      when 5..11
+                        "Good Morning <i class='fa fa-coffee'></i>".html_safe
+                      when 12..15
+                        "Good Afternoon"
+                      when 16..19
+                        ["Good Evening", "Working Hard!"].sample
+                      when 20..23, 0..4
+                         "Goodnight <i class='fa fa-moon-o'></i>".html_safe
+                      end    
+    date = Date.current 
+    @current_date = date.stamp("Sun Aug 16th")
+                      
+  end
+end
